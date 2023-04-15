17b2d787b31fa1d26f2da05b4df1e77df3bf7c36
Kweeka <mtalbi29@yahoo.com>
2023-04-15 21:12:26 +0100

    Update step.tsx

--- multi-step-form/src/components/ui/Step.tsx
+++ multi-step-form/src/components/ui/Step.tsx
@@ -7,7 +7,7 @@ type StepProps = {
 const Step: React.FC<StepProps> = ({ stepNumber, stepDescription, selected }) => {
   return (
     <div className="w-full relative">
-      <div className={`absolute w-[33px] h-[33px] rounded-full flex justify-center items-center top-0 ${selected ? 'bg-cyan-100' : 'bg-none border border-cyan-100 text-white'}`}>{stepNumber}</div>
+      <div className={`absolute w-[33px] h-[33px] font-ubuntu rounded-full flex justify-center items-center top-0 ${selected ? 'bg-cyan-100' : 'bg-none border border-cyan-100 text-white'}`}>{stepNumber}</div>
       <div className="w-full pl-[50px]">
         <p className="text-xs font-ubuntu-md uppercase text-slate-300">step {stepNumber}</p>
         <p className="text-sm font-ubuntu-bd uppercase text-white">{stepDescription}</p>
