30d4f42b78078dadd24a678628fafa47a8498dbb
kweeka <mtalbi29@yahoo.com>
2023-04-18 11:44:50 +0100

    Add styles for invalid input

--- multi-step-form/src/components/ui/Field.tsx
+++ multi-step-form/src/components/ui/Field.tsx
@@ -1,16 +1,21 @@
 interface FieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
 	label: string;
 	id: string;
+	errorMessage?: string;
+	props?: React.InputHTMLAttributes<HTMLInputElement>
 }
 
-const Field: React.FC<FieldProps> = ({ label, id }, ...props) => {
+const Field: React.FC<FieldProps> = ({ label, errorMessage, id, ...props}) => {
   return (
 		<div className="w-full">
 			<label htmlFor={id} className="block text-sm font-medium text-gray-700">
 				{label}
 			</label>
-			<div id={id} className="mt-2">
-				<input {...props} className="shadow-sm pl-3 py-[0.6rem] text-slate-600 font-ubuntu-bd border focus:border-gray-400 focus-visible:ring-gray-400 focus-visible:border-gray-400 block w-full sm:text-sm border-gray-300 rounded-md invalid:border invalid:border-red-300" />
+			<div id={id} className="mt-2 relative">
+				<input {...props} className="shadow-sm peer pl-3 py-[0.6rem] text-slate-600 font-ubuntu-bd border focus:border-gray-400 focus-visible:ring-gray-400 focus-visible:border-gray-400 block w-full sm:text-sm rounded-md border-gray-300 invalid:border-red-500" />
+				<span className="invisible absolute right-0 top-[-28px] peer-invalid:visible text-red-500 font-ubuntu-bd text-sm m-0 p-0">
+					{errorMessage}
+				</span>
 			</div>
 		</div>
 	)
