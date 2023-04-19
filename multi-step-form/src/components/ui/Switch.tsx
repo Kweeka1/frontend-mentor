9e45f32bcb5ef7ed36977a7b77ba2ef3707e719f
Kweeka1 <mtalbi29@yahoo.com>
2023-04-19 11:48:56 +0100

    Add switch component

---
+++ multi-step-form/src/components/ui/Switch.tsx
@@ -0,0 +1,22 @@
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+import { forwardRef } from "react";
+
+const Switch = forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ ...props }, ref) => (
+  <SwitchPrimitives.Root
+    className="peer inline-flex h-[18px] w-[38px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-800 data-[state=unchecked]:bg-slate-800"
+    {...props}
+    ref={ref}
+  >
+    <SwitchPrimitives.Thumb
+      className="pointer-events-none block h-3 w-3 rounded-full bg-white shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-[1px]"
+    />
+  </SwitchPrimitives.Root>
+))
+
+Switch.displayName = SwitchPrimitives.Root.displayName
+
+export default Switch;
+
