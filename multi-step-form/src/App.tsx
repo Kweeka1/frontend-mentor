a6cf80a825b0af821778fef3c564131e84c6e431
Kweeka1 <mtalbi29@yahoo.com>
2023-04-20 12:52:23 +0100

    Delete boilerplate code

--- multi-step-form/src/App.tsx
+++ multi-step-form/src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import Form from './components/form/Form'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="w-full h-full flex justify-center items-center">
       <Form />
