759c3ed9534792ad8f0f146dca37dfdcfe46a7ed
Kweeka1 <mtalbi29@yahoo.com>
2023-04-20 12:54:39 +0100

    Add functionality to Switch and Cards

--- multi-step-form/src/components/plan/Plan.tsx
+++ multi-step-form/src/components/plan/Plan.tsx
@@ -1,8 +1,19 @@
 import QualityCard from "../ui/QualityCard";
 import Switch from "../ui/Switch";
 
-const Plan: React.FC<SubFormProps> = ({ form, handleChange }) => {
-  const { frequency, price } = form;
+interface PlanProps {
+  form: IForm;
+  onSwitchChange: () => void;
+  onPlanChange: (type: QualityTypes) => void;
+}
+
+const Plan: React.FC<PlanProps> = ({
+  form,
+  onSwitchChange,
+  onPlanChange,
+}) => {
+  const { isYearlyPlan, quality, price } = form;
+  const frequency: Frequency = isYearlyPlan ? "yearly" : "monthly";
 
   return (
     <div className="flex flex-col">
@@ -14,18 +25,48 @@ const Plan: React.FC<SubFormProps> = ({ form, handleChange }) => {
       </p>
       <div className="py-8 flex flex-col gap-5">
         <div className="flex justify-between">
-          <QualityCard price={price} frequency={frequency} quality="arcade" />
-          <QualityCard price={price} frequency={frequency} quality="advanced" />
-          <QualityCard price={price} frequency={frequency} quality="pro" />
+          <QualityCard
+            type="arcade"
+            onClick={() => onPlanChange("arcade")}
+            price={price}
+            frequency={frequency}
+            quality={quality}
+          />
+          <QualityCard
+            type="advanced"
+            onClick={() => onPlanChange("advanced")}
+            price={price}
+            frequency={frequency}
+            quality={quality}
+          />
+          <QualityCard
+            type="pro"
+            onClick={() => onPlanChange("pro")}
+            price={price}
+            frequency={frequency}
+            quality={quality}
+          />
         </div>
         <div className="flex gap-4 justify-center items-center p-4 bg-gray-100 rounded-md">
-          <p className="text-slate-700 font-ubuntu-md">Monthly</p>
-          <Switch  />
-          <p className="text-slate-700 font-ubuntu-md">Yearly</p>
+          <p
+            className={`${
+              isYearlyPlan ? "text-slate-400" : "text-slate-700"
+            } font-ubuntu-md`}
+          >
+            Monthly
+          </p>
+          <Switch onCheckedChange={onSwitchChange} />
+          <p
+            className={`${
+              isYearlyPlan ? "text-slate-700" : "text-slate-400"
+            } font-ubuntu-md`}
+          >
+            Yearly
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Plan;
