e4a04a314f726de94a7554a83b9758fe5884e5e6
Kweeka1 <mtalbi29@yahoo.com>
2023-04-20 12:54:58 +0100

    Add ability to select a card

--- multi-step-form/src/components/ui/QualityCard.tsx
+++ multi-step-form/src/components/ui/QualityCard.tsx
@@ -1,18 +1,21 @@
-interface QualityCardProps {
-  quality: Quality;
+interface QualityCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  quality?: QualityTypes;
+  type: QualityTypes;
   price: Price;
   frequency: Frequency;
 }
 
-const QualityCard: React.FC<QualityCardProps> = ({ quality, price, frequency }) => {
+const QualityCard: React.FC<QualityCardProps> = ({ quality, type, price, frequency, ...props }) => {
+  const isSelected = type === quality;
+
   return (
-    <div className="h-[155px] w-[130px] cursor-pointer hover:border-violet-800 duration-150 hover:bg-cyan-50 ease-in flex flex-col justify-between items-start p-4 rounded-md border border-gray-300">
+    <div {...props} className={`h-[155px] w-[130px] cursor-pointer hover:border-violet-800 duration-150 hover:bg-slate-100 ease-in flex flex-col justify-between items-start p-4 rounded-md border ${isSelected ? 'border-violet-800 bg-slate-100' : 'border-gray-300'}`}>
       <div className="w-[40px] h-[40px]">
-        <img className="w-full h-full" src={`/images/icon-${quality}.svg`} alt={quality} />
+        <img className="w-full h-full" src={`/images/icon-${type}.svg`} alt={type} />
       </div>
       <div>
-        <p className="font-ubuntu-bd text-light text-slate-700">{quality[0].toUpperCase() + quality.slice(1)}</p>
-        <p className="font-ubuntu-bd text-sm text-gray-400">${price[frequency].values[quality]}/{price[frequency].name}</p>
+        <p className="font-ubuntu-bd text-light text-slate-700">{type[0].toUpperCase() + type.slice(1)}</p>
+        <p className="font-ubuntu-md text-sm text-gray-400">${price[frequency].values[type]}/{price[frequency].name}</p>
       </div>
     </div>
   )
