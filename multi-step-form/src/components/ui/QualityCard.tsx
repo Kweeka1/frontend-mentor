72c858eca1df519f46bd24970d0d60b1d3b0e300
Kweeka1 <mtalbi29@yahoo.com>
2023-04-21 10:50:54 +0100

    Add months saved text

--- multi-step-form/src/components/ui/QualityCard.tsx
+++ multi-step-form/src/components/ui/QualityCard.tsx
@@ -1,6 +1,6 @@
 interface QualityCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  quality?: QualityTypes;
-  type: QualityTypes;
+  quality?: Quality;
+  type: Quality;
   price: Price;
   frequency: Frequency;
 }
@@ -9,13 +9,14 @@ const QualityCard: React.FC<QualityCardProps> = ({ quality, type, price, frequen
   const isSelected = type === quality;
 
   return (
-    <div {...props} className={`h-[155px] w-[130px] cursor-pointer hover:border-violet-800 duration-150 hover:bg-slate-100 ease-in flex flex-col justify-between items-start p-4 rounded-md border ${isSelected ? 'border-violet-800 bg-slate-100' : 'border-gray-300'}`}>
-      <div className="w-[40px] h-[40px]">
+    <div {...props} className={`min-h-[155px] w-[135px] cursor-pointer hover:border-violet-800 duration-150 hover:bg-slate-100 ease-in flex flex-col justify-between items-start pt-4 px-4 pb-3 rounded-md border ${isSelected ? 'border-violet-800 bg-slate-100' : 'border-gray-300'}`}>
+      <div className="w-[40px] h-[40px] mb-6">
         <img className="w-full h-full" src={`/images/icon-${type}.svg`} alt={type} />
       </div>
       <div>
-        <p className="font-ubuntu-bd text-light text-slate-700">{type[0].toUpperCase() + type.slice(1)}</p>
-        <p className="font-ubuntu-md text-sm text-gray-400">${price[frequency].values[type]}/{price[frequency].name}</p>
+        <p className="font-ubuntu-bd text-light text-slate-700 py-[2px]">{type[0].toUpperCase() + type.slice(1)}</p>
+        <p className="font-ubuntu-md text-sm text-gray-400 py-[2px]">${price[frequency].values[type]}/{price[frequency].name}</p>
+        { frequency === "yearly" && <p className="font-ubuntu text-sm text-slate-700 py-[2px]">2 months free</p> }
       </div>
     </div>
   )
